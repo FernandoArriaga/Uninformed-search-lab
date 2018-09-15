4196958e34445f95da14120c74012a4f7649f076
Fernando Arriaga <A01270913@itesm.mx>
2018-09-14 21:05:13 -0500

    Update UCS.cpp

--- UCS.cpp
+++ UCS.cpp
@@ -23,112 +23,9 @@ void cpc(void)
 friend class UCS;
 };
 
-unsigned int hn(statenode*)
+unsigned int hn(statenode* p)
 {
     return 0;
-}
-
-statenode* shift(statenode* node, int start, int finish)
-{
-statenode* next=new statenode;
-for(unsigned int i=0; i<node->stacks.size();i++)
-{
-next->stacks.push_back(node->stacks[i]);
-}
-next->stacks[finish].push(next->stacks[start].top());
-next->stacks[start].pop();
-next->parent=node;
-next->Nstart=start;
-next->Nfinish=finish;
-next->cpc();
-return next;
-}
-
-class UCS {
-    public:
-unsigned int height;
-statenode* Instate;
-statenode* Goalstate;
-std::vector<statenode*> Pqueue;
-
-statenode* fn(void)
-{
-unsigned int lowestpathcost=4294967295;;
-    int j=0;
-    statenode* temp;
- for (unsigned int i=0;i<Pqueue.size();i++)
- {
-     if(((Pqueue[i]->pathcost)+hn(Pqueue[i]))<lowestpathcost)
-     {
-         lowestpathcost=((Pqueue[i]->pathcost)+hn(Pqueue[i]));
-         temp=Pqueue[i];
-         j=i;
-     }
- }
- Pqueue.erase(Pqueue.begin()+j);
- return temp;
-}
-
-void expand (statenode* p)
-{
-    statenode* bfer;
-for(unsigned int s=0;s < p->stacks.size();s++)
-{
-    for(unsigned int f=0;f < p->stacks.size();f++)
-    {
-        if (!(p->stacks[s].empty())&&(p->stacks[f].size() < this->height)&&(f!=s))
-        {
-
-            bfer=shift(p,s,f);
-            this->Pqueue.push_back(bfer);
-        }
-    }
-}
-
-}
-
-friend class statenode;
-};
-
-
-statenode* initialize(std::string buffer, unsigned int maxs){
-statenode* temp=new statenode;
-std::stack<char> sbuffer;
-int j=0;
-for(unsigned int i=0;i<buffer.length();i++)
-{
-    if(buffer[i]>='A' && buffer[i]<='z')
-    {
-    sbuffer.push(buffer[i]);
-    }
-
-    if(buffer[i]==';')
-    {
-    if(sbuffer.size()>maxs)
-    {
-        flag=1;
-        return temp;
-    }
-    temp->stacks.push_back(sbuffer);
-    j++;
-        while(!sbuffer.empty())
-        {
-        sbuffer.pop();
-        }
-    }
-}
-if(sbuffer.size()>maxs)
-    {
-        flag=1;
-        return temp;
-    }
-temp->stacks.push_back(sbuffer);
-
-temp->parent=0;
-temp->Nstart=0;
-temp->Nfinish=0;
-temp->pathcost=0;
-return temp;
 }
 
 
@@ -182,6 +79,125 @@ goal.stacks.push_back(gs->stacks[i]);
     }
     return true;
 }
+
+statenode* shift(statenode* node, int start, int finish)
+{
+statenode* next=new statenode;
+for(unsigned int i=0; i<node->stacks.size();i++)
+{
+next->stacks.push_back(node->stacks[i]);
+}
+next->stacks[finish].push(next->stacks[start].top());
+next->stacks[start].pop();
+next->parent=node;
+next->Nstart=start;
+next->Nfinish=finish;
+next->cpc();
+return next;
+}
+
+class UCS {
+    public:
+unsigned int height;
+statenode* Instate;
+statenode* Goalstate;
+std::vector<statenode*> Pqueue;
+std::vector<statenode*> Visited;
+
+
+statenode* fn(void)
+{
+unsigned int lowestpathcost=4294967295;;
+    int j=0;
+    statenode* temp;
+ for (unsigned int i=0;i<Pqueue.size();i++)
+ {
+     if(((Pqueue[i]->pathcost)+hn(Pqueue[i]))<lowestpathcost)
+     {
+         lowestpathcost=((Pqueue[i]->pathcost)+hn(Pqueue[i]));
+         temp=Pqueue[i];
+         j=i;
+     }
+ }
+ Pqueue.erase(Pqueue.begin()+j);
+ Visited.push_back(temp);
+ return temp;
+}
+
+bool visit (statenode* p)
+{
+    for (unsigned int i =0;i < Visited.size();i++)
+    {
+        if (comp(Visited[i],p))
+            return true;
+    }
+    return false;
+}
+
+void expand (statenode* p)
+{
+    statenode* bfer;
+for(unsigned int s=0;s < p->stacks.size();s++)
+{
+    for(unsigned int f=0;f < p->stacks.size();f++)
+    {
+        if (!(p->stacks[s].empty())&&(p->stacks[f].size() < this->height)&&(f!=s))
+        {
+
+            bfer=shift(p,s,f);
+            if(!(visit(bfer)))
+            this->Pqueue.push_back(bfer);
+        }
+    }
+}
+
+}
+
+friend class statenode;
+};
+
+
+statenode* initialize(std::string buffer, unsigned int maxs){
+statenode* temp=new statenode;
+std::stack<char> sbuffer;
+int j=0;
+for(unsigned int i=0;i<buffer.length();i++)
+{
+    if(buffer[i]>='A' && buffer[i]<='z')
+    {
+    sbuffer.push(buffer[i]);
+    }
+
+    if(buffer[i]==';')
+    {
+    if(sbuffer.size()>maxs)
+    {
+        flag=1;
+        return temp;
+    }
+    temp->stacks.push_back(sbuffer);
+    j++;
+        while(!sbuffer.empty())
+        {
+        sbuffer.pop();
+        }
+    }
+}
+if(sbuffer.size()>maxs)
+    {
+        flag=1;
+        return temp;
+    }
+temp->stacks.push_back(sbuffer);
+
+temp->parent=0;
+temp->Nstart=0;
+temp->Nfinish=0;
+temp->pathcost=0;
+return temp;
+}
+
+
 
 void solution(statenode* temp)
 {
